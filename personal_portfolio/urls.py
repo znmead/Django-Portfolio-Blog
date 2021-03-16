9bc178eaacb3aa0d29e945b152efa4bfdaf159e9
Zach Mead <zachary.n.mead@gmail.com>
2021-03-15 21:54:19 -0500

    Models?

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello_world.urls')),
+    # path('projects/', views.projects, name='projects'),
+    
 ]
