f33cf4871a54e2c230dbaeeecfd5edddc87122da
Zach Mead <zachary.n.mead@gmail.com>
2021-03-20 17:38:21 -0500

    Project finished, projects and blog working

--- blog/views.py
+++ blog/views.py
@@ -24,17 +24,18 @@ def blog_category(request, category):
     }
     return render(request, "blog_category.html", context)
 
-    def blog_detail(request, pk):
-        post = Post.objects.get(pk=pk)
 
-        form = CommentForm()
-        if request.method == 'POST':
-            form = CommentForm(request.POST)
-            if form.is_valid():
-                comment = Comment(
-                    author=form.cleaned_data["author"],
-                    body=form.cleaned_data["body"],
-                    post=post
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
             )
             comment.save()
 
